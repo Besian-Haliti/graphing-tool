6e377860975343074d801fe50da61235704af44b
Besian Haliti <besian.haliti@hotmail.com>
2024-12-07 05:12:11 +0000

    query modification2

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import GraphTool from "./component/GraphTool/GraphTool";
 import PromptTool from "./component/TrainingTool/PromptTool";
-import './hometool.css';
 
 export default function Home() {
   return (
